5c44e44427499b64e5c57e8de832b1b68629d1a1
Ratnesh Mallah <contactratnesh2050@gmail.com>
2020-01-29 18:39:36 +0530

    Add files via upload

--- BinaryTree.py
+++ BinaryTree.py
@@ -4,24 +4,84 @@ class Node:
         self.leftChild = None
         self.rightChild = None
 
-class BST:
+class BST(object):
     def __init__(self):
         self.root = None
     
-    def insertData(self,data):
+    # O(log(n))
+    def insert(self,data):
         if not self.root:
             self.root = Node(data)
         
         else:
-            if data < self.root.data:
-                self.insertOnleftChild(data, self.root)
+            self.insertNode(data,self.root)
+
+    def insertNode(self, data, node):
+        if data < node.data:
+            if not node.leftChild:
+                node.leftChild = Node(data)
             else:
-                self.insertOnrightChild(data, self.root)
+                return self.insertNode(data,node.leftChild)
+        else:
+            if not node.rightChild:
+                node.rightChild = Node(data)
+            else:
+                return self.insertNode(data, node.rightChild)
 
-    def insertOnleftChild(self,data,node):
-        pass
-    
 
-    def insertOnrightChild(self,data,node):
-        pass
+    def getMinValue(self):
+        if self.root:
+            return self.get_min(self.root)
 
+
+    def get_min(self,node):
+        if node.leftChild:
+            return self.get_min(node.leftChild)
+
+        return node.data
+
+
+
+    def getMaxValue(self):
+        if self.root:
+            return self.get_max(self.root)
+
+
+    def get_max(self,node):
+        if node.rightChild:
+            return self.get_max(node.rightChild)
+
+        return node.data
+
+
+    def traverse(self):
+        if self.root:
+            return self.traverseInOrder(self.root)
+
+
+    def traverseInOrder(self, node):
+        if node.leftChild:
+            return self.traverseInOrder(node.leftChild)
+
+        print("%s"%node.data)
+
+        if node.rightChild:
+            return self.traverseInOrder(node.rightChild)
+
+
+
+
+bst = BST()
+bst.insert(2)
+bst.insert(20)
+bst.insert(23)
+bst.insert(21)
+bst.insert(30)
+bst.insert(1)
+
+print("min V")
+bst.getMinValue()
+print("max v")
+bst.getMaxValue()
+print("traverse")
+bst.traverse()
